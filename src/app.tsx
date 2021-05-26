b44789874a8b3a4b8ad23c96d94ceae735fdc313
Elliot Nash <elliotnash@gmail.com>
2021-05-26 16:51:08 -0700

    add map componnt

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import Map from './components/map';
+
 function render() {
-  ReactDOM.render(<h2>Hello World!</h2>, document.body);
+  ReactDOM.render(
+    <div>
+      <Map/>
+    </div>, 
+    document.body
+  );
 }
 
 render();
---
+++ src/components/map.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+class Map extends React.Component {
+  render(): React.ReactNode {
+    return <h1>Hello, World!</h1>;
+  }
+}
+
+export default Map;
